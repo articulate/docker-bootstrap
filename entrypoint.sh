dddcdeed825a7d2eff87d5b06c617ae2f8f4d244
Dave York <dave.york@me.com>
2016-05-18 11:41:57 -0400

    variable for consul address

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ${VAULT_TOKEN+x} ]
 then
-  consul-template -consul=consul.articulate.zone:8500 -template=/exports.ctmpl:exports.sh -ssl-verify=false -once
+  consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:exports.sh -ssl-verify=false -once
   source exports.sh
 else
   echo "VAULT_TOKEN is not set skipping exports"
