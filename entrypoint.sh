afa799adb73542e93ec04be10c216dfb7bef3866
Dave York <dave.york@me.com>
2016-05-19 10:05:51 -0400

    put the exec back the way it was.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 if [ ${VAULT_TOKEN+x} ]
 then
   consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:/tmp/exports.sh -once
-  exec "source /tmp/exports.sh && $*"
+  source /tmp/exports.sh
 else
   echo "VAULT_TOKEN is not set skipping exports"
-  exec "$@"
 fi
+exec "$@"
