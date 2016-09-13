577414810a17b72981813ccd955b2f07f3456d8d
Dave York <dave.york@me.com>
2016-09-13 10:57:07 -0400

    add some informational logging
     - troubleshooting issues with bootstrap and env variables will be easier if we know things are happening like they should be.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ ${VAULT_TOKEN} ]
 then
+  echo "VAULT_TOKEN is present generating /tmp/exports.sh"
   consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:/tmp/exports.sh -once
   source /tmp/exports.sh
 else
