3b580f4e085550fe4660e46c001c6b14f0bb79a1
Dave York <dave.york@me.com>
2016-09-13 11:15:26 -0400

    output JSON for logstash parsing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [ ${VAULT_TOKEN} ]
 then
-  echo "VAULT_TOKEN is present generating /tmp/exports.sh"
+  echo '{"message": "VAULT_TOKEN is present generating /tmp/exports.sh"}'
   consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:/tmp/exports.sh -once
   source /tmp/exports.sh
 else
-  echo "VAULT_TOKEN is not set skipping exports"
+  echo '{"message": "VAULT_TOKEN is not set skipping exports"}'
 fi
 exec "$@"
