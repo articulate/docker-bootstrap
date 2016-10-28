01aaabdb12e3e7045b4c456a27c9775900236260
Brian Stolz <brian@tecnobrat.com>
2016-10-28 10:20:08 -0700

    Don't proceed on an error and show a message

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 if [ ${VAULT_TOKEN} ]
 then
-  consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:/tmp/exports.sh -once
-  source /tmp/exports.sh
+  if consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:/tmp/exports.sh -once
+  then
+    source /tmp/exports.sh
+  else
+    echo "======== CONSUL OR VAULT ARE HAVING ISSUES, WEB OPS HAVE BEEN ALERTED ========"
+    exit 1
+  fi
 else
   echo "VAULT_TOKEN is not set skipping exports"
 fi
