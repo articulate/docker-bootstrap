7e4136d33fb0f58c3a9c35883c65cc7c66515649
Luke van der Hoeven <hi@plukevdh.me>
2016-12-07 12:53:54 -0500

    kms

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ -f ".app.json" ]
+then
+  VAULT_TOKEN=$(jq -r '.vault_token' .app.json | base64 --decode | aws kms decrypt --ciphertext-blob fileb:///dev/stdin --output text --query Plaintext | base64 --decode)
+fi
+
 if [ ${VAULT_TOKEN} ]
 then
   if consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:/tmp/exports.sh -once -max-stale=0
@@ -9,6 +15,7 @@ then
     exit 1
   fi
 else
-  echo "VAULT_TOKEN is not set skipping exports"
+  echo "Vault token was not set. It may be missing from the .app.json or KMS may have been unable to decrypt."
+  exit 1
 fi
 exec "$@"
--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y unzip git
+apt-get install -y unzip git sudo python-dev jq
 rm -rf /var/lib/apt/lists/*
+
 git clone https://github.com/articulate/docker-consul-template-bootstrap.git
 wget https://releases.hashicorp.com/consul-template/0.14.0/consul-template_0.14.0_linux_amd64.zip
 unzip -d /usr/local/bin consul-template_0.14.0_linux_amd64.zip
+
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 mv ./docker-consul-template-bootstrap/entrypoint.sh /entrypoint.sh
 mv ./docker-consul-template-bootstrap/exports.ctmpl /exports.ctmpl
