044793686a34686ad9ef958e2dcdf0d6d106360b
Adam Ochonicki <fromonesrc@users.noreply.github.com>
2017-01-20 12:31:58 -0500

    Drop Vault requirement

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,16 @@ then
   export VAULT_TOKEN=$(echo $ENCRYPTED_VAULT_TOKEN | base64 --decode | aws kms decrypt --ciphertext-blob fileb:///dev/stdin --output text --query Plaintext --region $AWS_REGION | base64 --decode)
 fi
 
-if [ ${VAULT_TOKEN} ] && [ ${CONSUL_ADDR} ] && [ ${VAULT_ADDR} ]
+if [ ${CONSUL_ADDR} ]
 then
   if consul-template -consul=$CONSUL_ADDR -template=/exports.ctmpl:/tmp/exports.sh -once -max-stale=0
   then
     source /tmp/exports.sh
   else
-    echo "======== Consul or Vault are misbehaving. If you are seeing this in prod, Engineering Ops have been alerted. ========"
+    echo "======== Consul may be misbehaving. If you are seeing this in prod, Engineering Ops have been alerted. ========"
     exit 1
   fi
 else
-  echo "VAULT_TOKEN, VAULT_ADDR, or CONSUL_ADDR are not set skipping exports"
+  echo "CONSUL_ADDR are not set skipping exports"
 fi
 exec "$@"
