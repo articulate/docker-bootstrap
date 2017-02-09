c6e7167544d5783736897f7c60c79563466f4939
Adam Ochonicki <fromonesrc@users.noreply.github.com>
2017-02-09 12:40:33 -0500

    Revert "Drop Vault requirement"

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apt-get install -y unzip git sudo python-dev jq wget
 rm -rf /var/lib/apt/lists/*
 
 git clone https://github.com/articulate/docker-consul-template-bootstrap.git
-wget https://releases.hashicorp.com/consul-template/0.18.1/consul-template_0.18.1_linux_amd64.zip
+wget https://releases.hashicorp.com/consul-template/0.18.0-rc1/consul-template_0.18.0-rc1_linux_amd64.zip
 unzip -d /usr/local/bin consul-template_0.18.0-rc1_linux_amd64.zip
 
 wget "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
@@ -13,5 +13,5 @@ unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 mv ./docker-consul-template-bootstrap/entrypoint.sh /entrypoint.sh
-mv ./docker-consul-template-bootstrap/export-consul.ctmpl /export-consul.ctmpl
-mv ./docker-consul-template-bootstrap/export-vault.ctmpl /export-vault.ctmpl
+mv ./docker-consul-template-bootstrap/exports.ctmpl /exports.ctmpl
+
