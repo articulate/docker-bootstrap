f3f37f57b753727160bd42f260716a700f102a9a
Luke van der Hoeven <hi@plukevdh.me>
2017-02-10 10:42:48 -0500

    cleanup some install deets

--- install.sh
+++ install.sh
@@ -4,14 +4,17 @@ apt-get update
 apt-get install -y unzip git sudo python-dev jq wget
 rm -rf /var/lib/apt/lists/*
 
-export CONSUL_TEMPLATE_VERSION=0.8.1
-git clone https://github.com/articulate/docker-consul-template-bootstrap.git
-wget https://releases.hashicorp.com/consul-template/$CONSUL_TEMPLATE_VERSION/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip
-unzip -d /usr/local/bin consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip
+export CONSUL_TEMPLATE_VERSION=0.18.1
+wget -O consul-template.zip https://releases.hashicorp.com/consul-template/${CONSUL_TEMPLATE_VERSION}/consul-template_${CONSUL_TEMPLATE_VERSION}_linux_amd64.zip
+unzip -d /usr/local/bin consul-template.zip
+rm consul-template.zip
 
 wget "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+rm awscli-bundle.zip
+
+git clone https://github.com/articulate/docker-consul-template-bootstrap.git
 
 mv ./docker-consul-template-bootstrap/entrypoint.sh /entrypoint.sh
 mv ./docker-consul-template-bootstrap/export-consul.ctmpl /export-consul.ctmpl
