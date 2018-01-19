b15ba188f424222e6c650d0966707d5435aeaed4
Brian Stolz <brian@tecnobrat.com>
2018-01-19 14:51:56 -0800

    Full test suite, maybe...

--- spec/Dockerfile_spec.rb
+++ spec/Dockerfile_spec.rb
@@ -5,14 +5,14 @@ distros = ["debian", "centos", "alpine"]
 distros.each do |distro|
   describe "Dockerfile" do
     describe docker_build_template(template: "spec/dockerfiles/Dockerfile.#{distro}.erb", tag: "consul_template_bootstrap_#{distro}") do
-      [:consul, :vault].each do |backend_type|
-        describe backend_type do
-          describe "General sets work" do
-            set_var(backend_type, :global, "GLOBAL_VAR", "global-var")
-            set_var(backend_type, :product, "PRODUCT_VAR", "product-var")
-            set_var(backend_type, :service, "SERVICE_VAR", "service-var")
+      describe docker_run_with_envs("consul_template_bootstrap_#{distro}") do
+        [:consul, :vault].each do |backend_type|
+          describe backend_type do
+            describe "General sets work" do
+              set_var(backend_type, :global, "GLOBAL_VAR", "global-var")
+              set_var(backend_type, :product, "PRODUCT_VAR", "product-var")
+              set_var(backend_type, :service, "SERVICE_VAR", "service-var")
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "GLOBAL_VAR", "global-var" }
                 its(:stdout) { should include_env "PRODUCT_VAR", "product-var" }
@@ -20,125 +20,155 @@ distros.each do |distro|
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          describe "Products override global" do
-            set_var(backend_type, :global, "TEST_VAR", "global-var")
-            set_var(backend_type, :product, "TEST_VAR", "product-var")
+            describe "Products override global" do
+              set_var(backend_type, :global, "TEST_VAR", "global-var")
+              set_var(backend_type, :product, "TEST_VAR", "product-var")
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "product-var" }
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          describe "Services override products" do
-            set_var(backend_type, :product, "TEST_VAR", "product-var")
-            set_var(backend_type, :service, "TEST_VAR", "service-var")
+            describe "Services override products" do
+              set_var(backend_type, :product, "TEST_VAR", "product-var")
+              set_var(backend_type, :service, "TEST_VAR", "service-var")
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "service-var" }
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          describe "Services override products" do
-            set_var(backend_type, :global, "TEST_VAR", "global-var")
-            set_var(backend_type, :service, "TEST_VAR", "service-var")
+            describe "Services override products" do
+              set_var(backend_type, :global, "TEST_VAR", "global-var")
+              set_var(backend_type, :service, "TEST_VAR", "service-var")
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "service-var" }
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          describe "Services override globals" do
-            set_var(backend_type, :global, "TEST_VAR", "global-var")
-            set_var(backend_type, :service, "TEST_VAR", "service-var")
+            describe "Services override globals" do
+              set_var(backend_type, :global, "TEST_VAR", "global-var")
+              set_var(backend_type, :service, "TEST_VAR", "service-var")
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "service-var" }
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          describe "Services override products and globals" do
-            set_var(backend_type, :global, "TEST_VAR", "global-var")
-            set_var(backend_type, :product, "TEST_VAR", "product-var")
-            set_var(backend_type, :service, "TEST_VAR", "service-var")
+            describe "Services override products and globals" do
+              set_var(backend_type, :global, "TEST_VAR", "global-var")
+              set_var(backend_type, :product, "TEST_VAR", "product-var")
+              set_var(backend_type, :service, "TEST_VAR", "service-var")
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "service-var" }
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          ## LEGACY BACKWARDS COMPATIBLE
-          describe "New services space overrides old apps" do
-            set_var(backend_type, :service, "TEST_VAR", "service-var")
-            set_var(backend_type, :service, "TEST_VAR", "app-var", false)
+            ## LEGACY BACKWARDS COMPATIBLE
+            describe "New services space overrides old apps" do
+              set_var(backend_type, :service, "TEST_VAR", "service-var")
+              set_var(backend_type, :service, "TEST_VAR", "app-var", old_keys: true)
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "service-var" }
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          describe "New global space overrides old global" do
-            set_var(backend_type, :global, "TEST_VAR", "global-var")
-            set_var(backend_type, :global, "TEST_VAR", "old-global-var", false)
+            describe "New global space overrides old global" do
+              set_var(backend_type, :global, "TEST_VAR", "global-var")
+              set_var(backend_type, :global, "TEST_VAR", "old-global-var", old_keys: true)
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "global-var" }
                 its(:stderr) { should be_empty }
               end
             end
-          end
 
-          describe "Old apps override both global spaces" do
-            set_var(backend_type, :global, "TEST_VAR", "global-var")
-            set_var(backend_type, :global, "TEST_VAR", "old-global-var", false)
-            set_var(backend_type, :service, "TEST_VAR", "app-var", false)
+            describe "Old apps override both global spaces" do
+              set_var(backend_type, :global, "TEST_VAR", "global-var")
+              set_var(backend_type, :global, "TEST_VAR", "old-global-var", old_keys: true)
+              set_var(backend_type, :service, "TEST_VAR", "app-var", old_keys: true)
 
-            describe docker_run("consul_template_bootstrap_#{distro}") do
               describe entrypoint_command("env") do
                 its(:stdout) { should include_env "TEST_VAR", "app-var" }
+                its(:stdout) { should_not include_env "TEST_VAR", "old-global-var" }
+                its(:stdout) { should_not include_env "TEST_VAR", "global-var" }
                 its(:stderr) { should be_empty }
               end
             end
           end
+        end
+      end
 
-          describe "dev" do
-            add_env_vars({
-              "SERVICE_ENV": "dev"
-            })
-            set :service_env, "dev"
+      describe docker_run_with_envs("consul_template_bootstrap_#{distro}", SERVICE_ENV: "dev", ALREADY_SET: "true") do
+        [:consul, :vault].each do |backend_type|
+          describe backend_type do
+            describe "General sets work" do
+              set_var(backend_type, :global, "GLOBAL_VAR", "global-var", service_env: "dev")
+              set_var(backend_type, :global, "OLD_GLOBAL_VAR", "old-global-var", service_env: "dev", old_keys: true)
+              set_var(backend_type, :product, "PRODUCT_VAR", "product-var", service_env: "dev")
+              set_var(backend_type, :service, "SERVICE_VAR", "service-var", service_env: "dev")
+              set_var(backend_type, :service, "OLD_SERVICE_VAR", "old-service-var", service_env: "dev", old_keys: true)
 
+              describe entrypoint_command("env") do
+                its(:stdout) { should include_env "SERVICE_ENV", "dev" }
+                its(:stdout) { should include_env "GLOBAL_VAR", "global-var" }
+                its(:stdout) { should include_env "PRODUCT_VAR", "product-var" }
+                its(:stdout) { should include_env "SERVICE_VAR", "service-var" }
+                its(:stdout) { should include_env "OLD_GLOBAL_VAR", "old-global-var" }
+                its(:stdout) { should include_env "OLD_SERVICE_VAR", "old-service-var" }
+                its(:stderr) { should be_empty }
+              end
+            end
+
+            describe "Already set envs are not overriden" do
+              set_var(backend_type, :global, "ALREADY_SET", "false", service_env: "dev")
+
+              describe entrypoint_command("env") do
+                its(:stdout) { should include_env "SERVICE_ENV", "dev" }
+                its(:stdout) { should_not include_env "ALREADY_SET", "false" }
+                its(:stdout) { should include_env "ALREADY_SET", "true" }
+                its(:stderr) { should be_empty }
+              end
+            end
+          end
+        end
+      end
+
+      describe docker_run_with_envs("consul_template_bootstrap_#{distro}", SERVICE_ENV: "peer", ALREADY_SET: "true") do
+        [:consul, :vault].each do |backend_type|
+          describe backend_type do
             describe "General sets work" do
               set_var(backend_type, :global, "GLOBAL_VAR", "global-var")
               set_var(backend_type, :product, "PRODUCT_VAR", "product-var")
               set_var(backend_type, :service, "SERVICE_VAR", "service-var")
 
-              describe docker_run("consul_template_bootstrap_#{distro}") do
-                describe entrypoint_command("env") do
-                  its(:stdout) { should include_env "GLOBAL_VAR", "global-var" }
-                  its(:stdout) { should include_env "PRODUCT_VAR", "product-var" }
-                  its(:stdout) { should include_env "SERVICE_VAR", "service-var" }
-                  its(:stderr) { should be_empty }
-                end
+              describe entrypoint_command("env") do
+                its(:stdout) { should include_env "SERVICE_ENV", "peer" }
+                its(:stdout) { should include_env "GLOBAL_VAR", "global-var" }
+                its(:stdout) { should include_env "PRODUCT_VAR", "product-var" }
+                its(:stdout) { should include_env "SERVICE_VAR", "service-var" }
+                its(:stderr) { should be_empty }
+              end
+            end
+
+            describe "Already set envs are not overriden" do
+              set_var(backend_type, :global, "ALREADY_SET", "false")
+
+              describe entrypoint_command("env") do
+                its(:stdout) { should include_env "SERVICE_ENV", "peer" }
+                its(:stdout) { should_not include_env "ALREADY_SET", "false" }
+                its(:stdout) { should include_env "ALREADY_SET", "true" }
+                its(:stderr) { should be_empty }
               end
             end
           end
@@ -147,7 +177,3 @@ distros.each do |distro|
     end
   end
 end
-
-# dev consul doesn't override set envs
-# peer uses stage consul
-# vault overrides consul
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,50 +12,61 @@ def docker_build_template(opts = {})
   docker_build(opts)
 end
 
-def add_env_vars(env_vars_hash={})
-  set :env, get_config(:env) + [env_vars_hash.to_a]
+def docker_run_with_envs(name, env_vars_hash={})
+  global_env_vars = {
+    "CONSUL_ADDR": ENV.fetch("CONSUL_ADDR"),
+    "VAULT_ADDR": ENV.fetch("VAULT_ADDR"),
+    "VAULT_TOKEN": ENV.fetch("VAULT_TOKEN"),
+    "VAULT_RENEW_TOKEN": "false",
+    "SERVICE_ENV": $service_env,
+    "SERVICE_NAME": $service_name,
+    "SERVICE_PRODUCT": $service_product
+  }
+  use_env_vars = global_env_vars.merge(env_vars_hash)
+  use_env_vars = use_env_vars.to_a
+
+  docker_run(name, env: use_env_vars)
 end
 
 def entrypoint_command(cmd)
   command("/entrypoint.sh #{cmd}")
 end
 
-def set_var(store_type, scope, key, value, new=true)
-  service_name = get_config(:service_name)
-  service_product = get_config(:service_product)
-  service_env = get_config(:service_env)
+def set_var(store_type, scope, key, value, opts={})
+  around(:each) do |example|
+    service_name = $service_name
+    service_product = $service_product
+    service_env = opts[:service_env] || $service_env
 
-  full_key = case scope
-             when :service
-               if new
-                 "services/#{service_name}/env_vars/#{key}"
-               else
-                 "apps/#{service_name}/#{service_env}/env_vars/#{key}"
+    full_key = case scope
+               when :service
+                 if opts[:old_keys]
+                   "apps/#{service_name}/#{service_env}/env_vars/#{key}"
+                 else
+                   "services/#{service_name}/env_vars/#{key}"
+                 end
+               when :global
+                 if opts[:old_keys]
+                   "global/#{service_env}/env_vars/#{key}"
+                 else
+                   "global/env_vars/#{key}"
+                 end
+               when :product
+                 "products/#{service_product}/env_vars/#{key}"
                end
-             when :global
-               if new
-                 "global/env_vars/#{key}"
-               else
-                 "global/#{service_env}/env_vars/#{key}"
-               end
-             when :product
-               "products/#{service_product}/env_vars/#{key}"
-             end
-  if store_type == :vault
-    full_key = "secret/#{full_key}"
-  end
+    if store_type == :vault
+      full_key = "secret/#{full_key}"
+    end
 
-  puts "Setting '#{full_key}' to value '#{value}' in #{store_type}"
-
-  before(:each) do
+    #puts "Setting '#{full_key}' to value '#{value}' in #{store_type}"
     if store_type == :vault
       Vault.logical.write(full_key, value: value)
     else
       Diplomat::Kv.put(full_key, value)
     end
-  end
 
-  after(:each) do
+    example.run
+
     if store_type == :vault
       Vault.logical.delete(full_key)
     else
@@ -72,30 +83,23 @@ RSpec::Matchers.define :include_env do |env_var, env_val|
   match do |stdout|
     stdout.include?("#{env_var}=#{env_val}\n")
   end
-  failure_message_when_negated do |actual|
+  failure_message_when_negated do |stdout|
+    "expected that #{env_var}=#{env_val} is not in #{stdout}"
+  end
+  failure_message do |stdout|
     "expected that #{env_var}=#{env_val} is in #{stdout}"
   end
 end
 
 RSpec.configure do |config|
-  set :docker_container_create_options, HostConfig: { NetworkMode: "container:#{`hostname`.strip}" }
-
-  set :service_name, "service-name"
-  set :service_env, "stage"
-  set :service_product, "product-name"
-
-  set :env, [
-    ["CONSUL_ADDR", ENV.fetch("CONSUL_ADDR")],
-    ["VAULT_ADDR", ENV.fetch("VAULT_ADDR")],
-    ["VAULT_TOKEN", ENV.fetch("VAULT_TOKEN")],
-    ["VAULT_RENEW_TOKEN", "false"],
-    ["SERVICE_ENV", get_config(:service_env)],
-    ["SERVICE_NAME", get_config(:service_name)],
-    ["SERVICE_PRODUCT", get_config(:service_product)]
-  ]
-
   config.before(:suite) do
     Vault.logical.delete("secret")
     Diplomat::Kv.delete("", recurse: true)
   end
+
+  $service_name = "service-name"
+  $service_env = "stage"
+  $service_product = "product-name"
+
+  set :docker_container_create_options, "HostConfig": { "NetworkMode": "container:#{`hostname`.strip}" }
 end
