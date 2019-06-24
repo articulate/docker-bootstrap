5300cc7c280de6d0e5443cefadcaad64e7dd3244
Robin Horton <paklids@gmail.com>
2019-06-24 11:27:20 -0700

    installing python3-setuptools does not install pip3

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ fi
 
 if [ `command -v apt-get` ]; then
   apt-get update
-  apt-get -y install --no-install-recommends unzip sudo python3-setuptools jq wget curl
+  apt-get -y install --no-install-recommends unzip sudo python3-setuptools python3-pip jq wget curl
   pip3 --no-cache-dir install awscli
   apt-get clean && apt-get autoclean && apt-get -y autoremove --purge 
   rm -rf /var/lib/apt/lists/* /usr/share/doc /root/.cache/
