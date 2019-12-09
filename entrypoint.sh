1156d207a0c6547a9c407767675d7947ddd479d3
Purrbiscuit <spurr@articulate.com>
2019-12-09 11:02:17 -0500

    skip all vault bootstrap steps if VAULT_ADDR isnt set

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,51 +33,55 @@ else
   (>&2 echo "CONSUL_ADDR are not set skipping Consul exports")
 fi
 
-if [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]
-then
-  KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-  vault_token="null"
-  attempts=1
-  while [ "${attempts}" -le 10 ]; do
-    vault_token=$(curl -s --show-error --request POST \
-      --data '{"jwt": "'"$KUBE_TOKEN"'", "role": "'"$SERVICE_NAME"'"}' \
-      $VAULT_ADDR/v1/auth/kubernetes/login | jq -r '.auth.client_token');
-    if [[ "${vault_token}" != "null" ]]; then
-       break
-    fi
-    echo "Attempt number ${attempts} to get vault token failed, retrying..."
-    ((attempts+=1))
-    sleep 3
-  done
-
-  if [[ "${vault_token}" == "null" ]]; then
-    echo "Failed to get vault token via kubernetes"
-    exit 1
-  fi
-  export VAULT_TOKEN=${vault_token}
-fi
-
-if [ "${ENCRYPTED_VAULT_TOKEN}" ] && [ ! "${VAULT_TOKEN}" ]
-then
-  export VAULT_TOKEN=$(echo $ENCRYPTED_VAULT_TOKEN | base64 -d | aws kms decrypt --ciphertext-blob fileb:///dev/stdin --output text --query Plaintext --region $AWS_REGION | base64 -d)
-fi
-
-if [ ! "${VAULT_TOKEN}" ] && [ "${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}" ]
-then
-  export VAULT_TOKEN=$(vault login -method=aws -token-only)
-fi
-
-if [ ${VAULT_TOKEN} ] && [ ${CONSUL_ADDR} ] && [ ${VAULT_ADDR} ]
-then
-  if consul-template -consul-addr=$CONSUL_ADDR -vault-addr=$VAULT_ADDR -template=/consul-template/${CT_SERVICE_ENV}/export-vault.ctmpl:/tmp/export-vault.sh -once -max-stale=0
+if [ ${VAULT_ADDR} ]
+  if [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]
   then
-    source /tmp/export-vault.sh
+    KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+    vault_token="null"
+    attempts=1
+    while [ "${attempts}" -le 10 ]; do
+      vault_token=$(curl -s --show-error --request POST \
+        --data '{"jwt": "'"$KUBE_TOKEN"'", "role": "'"$SERVICE_NAME"'"}' \
+        $VAULT_ADDR/v1/auth/kubernetes/login | jq -r '.auth.client_token');
+      if [[ "${vault_token}" != "null" ]]; then
+        break
+      fi
+      echo "Attempt number ${attempts} to get vault token failed, retrying..."
+      ((attempts+=1))
+      sleep 3
+    done
+
+    if [[ "${vault_token}" == "null" ]]; then
+      echo "Failed to get vault token via kubernetes"
+      exit 1
+    fi
+    export VAULT_TOKEN=${vault_token}
+  fi
+
+  if [ "${ENCRYPTED_VAULT_TOKEN}" ] && [ ! "${VAULT_TOKEN}" ]
+  then
+    export VAULT_TOKEN=$(echo $ENCRYPTED_VAULT_TOKEN | base64 -d | aws kms decrypt --ciphertext-blob fileb:///dev/stdin --output text --query Plaintext --region $AWS_REGION | base64 -d)
+  fi
+
+  if [ ! "${VAULT_TOKEN}" ] && [ "${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}" ]
+  then
+    export VAULT_TOKEN=$(vault login -method=aws -token-only)
+  fi
+
+  if [ ${VAULT_TOKEN} ] && [ ${CONSUL_ADDR} ]
+  then
+    if consul-template -consul-addr=$CONSUL_ADDR -vault-addr=$VAULT_ADDR -template=/consul-template/${CT_SERVICE_ENV}/export-vault.ctmpl:/tmp/export-vault.sh -once -max-stale=0
+    then
+      source /tmp/export-vault.sh
+    else
+      (>&2 echo "Vault $MISBEHAVING_NOTICE")
+      exit 1
+    fi
   else
-    (>&2 echo "Vault $MISBEHAVING_NOTICE")
-    exit 1
+    (>&2 echo "VAULT_TOKEN or VAULT_ADDR are not set, skipping Vault exports")
   fi
 else
-  (>&2 echo "VAULT_TOKEN or VAULT_ADDR are not set, skipping Vault exports")
+    (>&2 echo "VAULT_ADDR is not set, skipping Vault exports")
 fi
 
 rm -f /tmp/export-vault.sh
