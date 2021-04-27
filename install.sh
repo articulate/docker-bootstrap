221af78f3113f394b1450ad3ab02afda99c356df
Tyler O'Donnell <tyler@tylerodonnell.com>
2021-04-27 16:25:35 -0400

    Remove sudo

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ fi
 # Install awscli
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip -q awscliv2.zip
-sudo ./aws/install
+./aws/install
 rm -r awscliv2.zip aws
 
 # Install Consul template
