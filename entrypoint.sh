28a4e8d43a07dcdc62253f55bc2fdb26f354c083
Matt Loberg <mloberg@articulate.com>
2022-05-12 12:26:25 -0500

    refactor(entrypoint): shellcheck improvements
    Make the entrypoint pass shellcheck. Use generic (env) bash for entrypoint. Make sure all output is
    sent to stderr and prefix with type of message (warn vs error)
    
    BREAKING CHANGE: remove deprecated APP_ config variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,93 +1,74 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 AWS_REGION="${AWS_REGION:-us-east-1}"
-if [ "${APP_ENV}" != "" ]; then
-  (>&2 echo "Using deprecated APP_ENV, please swap to SERVICE_ENV")
-  export SERVICE_ENV="${APP_ENV}"
-fi
-if [ "${APP_NAME}" != "" ]; then
-  (>&2 echo "Using deprecated APP_NAME, please swap to SERVICE_NAME")
-  export SERVICE_NAME="${APP_NAME}"
-fi
-if [ "${APP_PRODUCT}" != "" ]; then
-  (>&2 echo "Using deprecated APP_PRODUCT, please swap to SERVICE_PRODUCT")
-  export SERVICE_PRODUCT="${APP_PRODUCT}"
-fi
 
 # This will return everything before a - chararacter.
 # "peer-something-thing" => "peer"
 CT_SERVICE_ENV="${SERVICE_ENV%%-*}"
 
-MISBEHAVING_NOTICE="may be misbehaving. In a perfect world, our monitoring detected this problem and Platform Engineering was alerted... but just in case, please let us know."
-
-if [ ${CONSUL_ADDR} ]
-then
-  if consul-template -consul-addr=$CONSUL_ADDR -template=/consul-template/${CT_SERVICE_ENV}/export-consul.ctmpl:/tmp/export-consul.sh -once -max-stale=0
-  then
+if [ -n "$CONSUL_ADDR" ]; then
+  if consul-template -consul-addr="$CONSUL_ADDR" -template="/consul-template/${CT_SERVICE_ENV}/export-consul.ctmpl:/tmp/export-consul.sh" -once -max-stale=0; then
+    # shellcheck disable=SC1091
     source /tmp/export-consul.sh
+    rm -f /tmp/export-consul.sh
   else
-    (>&2 echo "Consul $MISBEHAVING_NOTICE")
+    (>&2 echo "ERROR: Unable to export from Consul")
     exit 1
   fi
 else
-  (>&2 echo "CONSUL_ADDR are not set skipping Consul exports")
+  (>&2 echo "WARN: CONSUL_ADDR is not set, skipping Consul exports")
 fi
 
-if [ ${VAULT_ADDR} ]
-  then
-  if [ ! "${VAULT_TOKEN}" ] && [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]
-  then
+if [ -n "$VAULT_ADDR" ]; then
+  if [ -z "$VAULT_TOKEN" ] && [ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]; then
     KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-    vault_token="null"
+    VAULT_TOKEN="null"
     attempts=1
     while [ "${attempts}" -le 10 ]; do
       response=$(curl -s --show-error --request POST \
         --data '{"jwt": "'"$KUBE_TOKEN"'", "role": "'"$SERVICE_NAME"'"}' \
-        $VAULT_ADDR/v1/auth/kubernetes/login)
-      vault_token=$(echo $response | jq -r '.auth.client_token');
-      if [[ "${vault_token}" != "null" ]]; then
-        break
-      fi
-      echo "Attempt number ${attempts} to get vault token failed, retrying..."
-      echo "Vault response: $(echo $response | jq '.errors[]')"
+        "${VAULT_ADDR}/v1/auth/kubernetes/login")
+      VAULT_TOKEN=$(echo "$response" | jq -r '.auth.client_token')
+      [[ "$VAULT_TOKEN" != "null" ]] && break
+      (>&2 echo "Attempt number ${attempts} to get vault token failed, retrying...")
+      (>&2 echo "Vault response: $(echo "$response" | jq '.errors[]')")
       ((attempts+=1))
       sleep 3
     done
 
-    if [[ "${vault_token}" == "null" ]]; then
+    if [[ "$VAULT_TOKEN" == "null" ]]; then
       echo "Failed to get vault token via kubernetes"
       exit 1
     fi
-    export VAULT_TOKEN=${vault_token}
+
+    export VAULT_TOKEN
   fi
 
-  if [ "${ENCRYPTED_VAULT_TOKEN}" ] && [ ! "${VAULT_TOKEN}" ]
-  then
-    export VAULT_TOKEN=$(echo $ENCRYPTED_VAULT_TOKEN | base64 -d | aws kms decrypt --ciphertext-blob fileb:///dev/stdin --output text --query Plaintext --region $AWS_REGION | base64 -d)
+  if [ -n "$ENCRYPTED_VAULT_TOKEN" ] && [ -z "$VAULT_TOKEN" ]; then
+    VAULT_TOKEN=$(echo "$ENCRYPTED_VAULT_TOKEN" | base64 -d | aws kms decrypt --ciphertext-blob fileb:///dev/stdin --output text --query Plaintext --region "$AWS_REGION" | base64 -d)
+    export VAULT_TOKEN
   fi
 
-  if [ ! "${VAULT_TOKEN}" ] && [ "${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}" ]
-  then
-    export VAULT_TOKEN=$(vault login -method=aws -token-only)
+  if [ -z "$VAULT_TOKEN" ] && [ -n "$AWS_CONTAINER_CREDENTIALS_RELATIVE_URI" ]; then
+    VAULT_TOKEN=$(vault login -method=aws -token-only)
+    export VAULT_TOKEN
   fi
 
-  if [ ${VAULT_TOKEN} ] && [ ${CONSUL_ADDR} ]
-  then
-    if consul-template -consul-addr=$CONSUL_ADDR -vault-addr=$VAULT_ADDR -template=/consul-template/${CT_SERVICE_ENV}/export-vault.ctmpl:/tmp/export-vault.sh -once -max-stale=0
-    then
+  if [ -n "$VAULT_TOKEN" ] && [ -n "$CONSUL_ADDR" ]; then
+    if consul-template -consul-addr="$CONSUL_ADDR" -vault-addr="$VAULT_ADDR" -template="/consul-template/${CT_SERVICE_ENV}/export-vault.ctmpl:/tmp/export-vault.sh" -once -max-stale=0; then
+      # shellcheck disable=SC1091
       source /tmp/export-vault.sh
+      rm -f /tmp/export-vault.sh
     else
-      (>&2 echo "Vault $MISBEHAVING_NOTICE")
+      (>&2 echo "ERROR: Unable to export from Vault")
       exit 1
     fi
   else
-    (>&2 echo "VAULT_TOKEN and/or CONSUL_ADDR not set, skipping Vault exports")
+    (>&2 echo "WARN: VAULT_TOKEN and/or CONSUL_ADDR not set, skipping Vault exports")
   fi
 else
-    (>&2 echo "VAULT_ADDR is not set, skipping Vault exports")
+  (>&2 echo "WARN: VAULT_ADDR is not set, skipping Vault exports")
 fi
-
-rm -f /tmp/export-vault.sh
-rm -f /tmp/export-consul.sh
 
 exec "$@"
